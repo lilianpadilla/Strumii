4edd42d70021f3f52e339b0cda89dceb6a13c2d2
Gael <ghernan5@trinity.edu>
2025-10-23 16:37:14 -0500

    Added calling the lesson db test data 2.

--- src/app/lesson-history/page.tsx
+++ src/app/lesson-history/page.tsx
@@ -17,14 +17,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Button } from "~/components/ui/button";
+
+import { caller } from "@/server/api/server"; 
 
 import { caller } from '~/server/api/server';
 
 export default async function Page() {
 
+<<<<<<< Updated upstream
     let lesson = await caller.lesson.getLesson();
     console.log(lesson);
+=======
+  let lessons = await caller.lesson.getLessons();
+>>>>>>> Stashed changes
 
     return (
         <>
@@ -52,7 +57,7 @@ export default async function Page() {
                 <b>Exclude Completed Lessons</b>
                 <Switch />
                  </div>
-                 <Card className= "w-full bg-[#93CAD7] text-white shadow">
+                 <Card className= "w-15/16 bg-[#93CAD7] text-white shadow">
                     <CardHeader>
                         <CardTitle>Statistics</CardTitle>
                         <CardDescription className="text-white">Time Played:</CardDescription>
@@ -60,6 +65,7 @@ export default async function Page() {
                         <CardDescription className="text-white">Songs Learned:</CardDescription>
                         </CardHeader>
                 </Card>
+<<<<<<< Updated upstream
                  <div className ="w-7/8 justify-center">
                 {/* make this a preset component later? */}
                 <a href="https://media.tenor.com/aSkdq3IU0g0AAAAM/laughing-cat.gif" className="block">
@@ -69,33 +75,71 @@ export default async function Page() {
                         <CardDescription>03-13-2025 : 2:12PM</CardDescription>
                         <CardDescription>10 Minute Lesson</CardDescription>
                         <CardDescription className="text-blue-500"><b>Review: Confident</b></CardDescription>
+=======
+                { /* to do: make the card change color depending on the confidence level of the lesson, if completed, do strumii blue! */ }
+                 <div className ="flex flex-col w-7/8 justify-center gap-3 pb-10">
+                 {lessons.map((lesson) => (
+                  <Card
+                    key = {lesson.id}
+                    // className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100"
+                    className={`cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 ${
+                        lesson.completed ? 'bg-[#93CAD7] text-white' : ''
+                    }`}
+                    >
+                      <CardHeader className="w-full">
+                        <CardTitle>{lesson.title}</CardTitle>
+                        <CardDescription>{lesson.description} </CardDescription>
+                        <CardDescription>Started: { lesson.createdAt.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', year: 'numeric', month: '2-digit', day: '2-digit', hour12: true}) } | 
+                            Completed: {lesson.completed ? /*{ lesson.createdAt.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', year: 'numeric', month: '2-digit', day: '2-digit', hour12: true}) }*/ 'Yes' : 'No'}
+                        </CardDescription>
+                        <CardDescription>{ lesson.expDuration } Minutes</CardDescription>
+                      </CardHeader>
+                    </Card>
+                 ))}
+                    <a href="https://media.tenor.com/aSkdq3IU0g0AAAAM/laughing-cat.gif" className="block">
+                    <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 bg-orange-500 text-white">
+                        <CardHeader>
+                            <CardTitle>Chord Progression: Em - D - C - B</CardTitle>
+                            <CardDescription className="text-white-500">03-13-2025 : 2:12PM</CardDescription>
+                            <CardDescription className="text-white-500">10 Minute Lesson</CardDescription>
+                            <CardDescription className="text-white-500"><b>Review: Not Confident</b></CardDescription>
+                            </CardHeader>
+                    </Card></a>
+                    <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 bg-[#93CAD7] text-white">
+                        <div className="flex items-center justify-between">
+                        <CardHeader className="w-full"> 
+                            <CardTitle>Chord: Em</CardTitle>
+                            <CardDescription className="text-white-500">03-13-2025 : 2:12PM</CardDescription>
+                            <CardDescription className="text-white-500">5 Minute Lesson</CardDescription>
+                            <CardDescription className="text-white-500"><b>Review: Completed!</b></CardDescription>
+>>>>>>> Stashed changes
                         </CardHeader>
-                </Card></a>
-                <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100">
-                    <CardHeader>
-                        <CardTitle>Chord Progression: Em - D - C - B</CardTitle>
-                        <CardDescription>03-13-2025 : 2:12PM</CardDescription>
-                        <CardDescription>10 Minute Lesson</CardDescription>
-                        <CardDescription className="text-red-500"><b>Review: Not Confident</b></CardDescription>
-                        </CardHeader>
-                </Card>
-                <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100">
-                    <CardHeader>
-                        <CardTitle>Chord: B</CardTitle>
-                        <CardDescription>03-13-2025 : 2:12PM</CardDescription>
-                        <CardDescription>5 Minute Lesson</CardDescription>
-                        <CardDescription className="text-yellow-500"><b>Review: Could Use Practice</b></CardDescription>
-                        </CardHeader>
-                </Card>
-                <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 bg-[#93CAD7] text-white">
-                    <CardHeader>
-                        <CardTitle>Chord: Em</CardTitle>
-                        {/* there HAS to be a better way to do this (maybe parameter?) */}
-                        <CardDescription className="text-white-500">03-13-2025 : 2:12PM</CardDescription>
-                        <CardDescription className="text-white-500">5 Minute Lesson</CardDescription>
-                        <CardDescription className="text-white-500"><b>Review: Confident</b></CardDescription>
-                        </CardHeader>
-                </Card>
+                            <div className="pr-5">
+                                <img src="/logo.png" alt=""/>
+                            </div>
+                        </div>
+                    </Card>
+                    <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 bg-green-500 text-white">
+                        <div className="flex items-center justify-between">
+                        <CardHeader className="w-full">
+                            <CardTitle>Chord: A</CardTitle>
+                            <CardDescription className="text-white-500">03-13-2025 : 2:12PM</CardDescription>
+                            <CardDescription className="text-white-500">5 Minute Lesson</CardDescription>
+                            <CardDescription className="text-white-500"><b>Review: Confident</b></CardDescription>
+                            </CardHeader>
+                            <div className="pr-5">
+                                <img src="/logo.png" alt=""/>
+                            </div>
+                        </div>
+                    </Card>
+                    <Card className="cursor-pointer hover:shadow-lg hover:scale-103 transition-transform duration-100 bg-red-500 text-white">
+                        <CardHeader>
+                            <CardTitle>Chord: Cm7</CardTitle>
+                            <CardDescription className="text-white-500">03-13-2025 : 2:12PM</CardDescription>
+                            <CardDescription className="text-white-500">5 Minute Lesson</CardDescription>
+                            <CardDescription className="text-white-500"><b>Review: Needs Practice</b></CardDescription>
+                            </CardHeader>
+                    </Card>
                 </div>
             </div>
         </>

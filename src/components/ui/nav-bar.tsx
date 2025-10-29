cc278440873e1a1240503492e25d47f0efe50172
edavalosanaya <davalosaeduardo@gmail.com>
2025-10-29 14:43:03 -0500

    Made small change.

--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -85,7 +85,7 @@ export default function Navbar(props: NavbarProps) {
             />
           </Link> */}
           <p className="font-[cursive] text-4xl">
-            Welcome Back!
+            Welcome Goodbye!
           </p>
 
           {/* Breadcrumbs */}

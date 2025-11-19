b06dec947f39d332dd4b13bffde9c06e67eec120
mtyree-trin <159827451+mtyree-trin@users.noreply.github.com>
2025-11-19 15:44:29 -0600

    Small navigation fix to buttons (#49)
    Co-authored-by: MolKatt <molly.tyree@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation"
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useAuth } from "~/providers/AuthProvider";
 import { Button } from "~/components/ui/button";
 import { DropdownMenuItem, DropdownMenuLabel } from "~/components/ui/dropdown-menu";
@@ -16,13 +17,15 @@ export default function Page() {
     <>
       <div className="flex flex-col w-full  ">
         <div className="h-full w-full justify-items-center pt-20 space-y-6">
-          <Button variant="secondary" className="flex justify-center w-4/5 h-80 text-2xl bg-[#93CAD7]" onClick={() => {router.push("/lesson-overview")}}> 
-            Start Lesson!
-          </Button> 
+           <Button variant="secondary" className="flex justify-center w-4/5 h-80 text-2xl bg-[#93CAD7]" onClick={() => {router.push('/lesson-overview')}}> 
+            Start Lesson
+           </Button>
           
-          <Button variant="secondary" className="flex justify-center w-4/5 h-40 text-2xl bg-[#93CAD7]" onClick={() => {router.push("/lesson-history")}}>
+          
+          <Button variant="secondary" className="flex justify-center w-4/5 h-40 text-2xl bg-[#93CAD7]" onClick={() => {router.push('/lesson-history')}}>
             Lesson History
           </Button> 
+         
         </div>
       </div>     
     </>
